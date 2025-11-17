a5cba693692011c5db8889b6859507558b90448f
Jim Posen <jim.posen@gmail.com>
2025-11-17 19:49:01 -0300

    Naive verification of attestation

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,42 @@ use alloy::primitives::{Address, Signature, address};
 use alloy::sol_types::SolType;
 use eyre::{Result, ensure};
 
-fn main() -> Result<()> {
-    let address = address!("0x664C7bA58aEE266307Cac0B5a8555095C1a4f7a0");
-    let message =
-        b"Ranked-choice voting is the single most important democracy reform needed in the US";
-    let signature = hex!(
-        "0x3ef5a537df46dc7113fd0d7bea4534315cac6238897b92dd52fd2fc2ff1bb9fb1b1e9f842836194b139f11e9c0d1edaf5fe32099690af3fc2b766b4f8d13cb111b"
-    );
+struct AttestedMessageInout<'a> {
+    attester_address: Address,
+    message: &'a [u8],
+    expected_gm: &'a str,
+}
 
-    verify_signature(&address, message, &signature)?;
+struct AttestedMessageWitness<'a> {
+    signer: Address,
+    message_signature: &'a [u8],
+    attestation_info: AttestationInfo,
+}
 
-    // Attestation schema: <https://base.easscan.org/schema/view/0x365f50a2e12159e29b002684b5c42a3c1ab67440b886b66ee9a4a9e8c8f7f528>
-    let attester_address = address!("0xc48117F22c8095504aFCa9795DCCbdA2BF5FBc73");
-    let eas_schema_id = hex!("0x365f50a2e12159e29b002684b5c42a3c1ab67440b886b66ee9a4a9e8c8f7f528");
-    let attestation_str = include_str!("../example-attest.json");
-    let expected_gm = "Binius";
+fn naively_verify_attested_message(
+    inout: AttestedMessageInout,
+    witness: AttestedMessageWitness,
+) -> Result<()> {
+    let AttestedMessageInout {
+        attester_address,
+        message,
+        expected_gm,
+    } = inout;
+    let AttestedMessageWitness {
+        signer,
+        message_signature,
+        attestation_info,
+    } = witness;
 
-    // Attestation link: <https://base.easscan.org/offchain/url/#attestation=eNqlkktuHDEMRO%2FS64FBUvwuPR3PJYIsKDV5gCABcvxoxhcI4tJCC1HFepS%2BH%2FgGb3jcDmcZe4M%2FTPAPInwVJxhdYILF7MLqfEkBZonzFO1eClmUlbNlVXdoYeu16yJg%2BcuEBkYRtegAih4Dhyp00VU8LvFwaVuYa7vhpd7YXD1tHxQy7djkT5%2FFjmgPouUQIsD5ODMs5Nt5zuud7g953Jd9YhYwR%2B4uYw1noL0qdmNKnzG1J%2Bczc6NLrERzHzaLXFNnoMrLZKi0QFIhoWyGmHsy%2BkRfTDl25qnGDNNdp2pVJGeUL29roU98VT5tvovnxwfpnoGdueAu6bIhQk5Mbks4bmg6GJWDb7BvHrdfP3%2FX0wK%2BJIIvSpl0P6yGiY3%2FMTg2Dz1BYs4rrb2vsaLXlVWKC9aYcoV66dBmS5FlRKNkfyIP6GGJq3Lx8eMvWFGrgQ%3D%3D>
-    let attestation_info = serde_json::from_str::<AttestationInfo>(attestation_str)?;
+    const EAS_SCHEMA_ID: [u8; 32] =
+        hex!("0x365f50a2e12159e29b002684b5c42a3c1ab67440b886b66ee9a4a9e8c8f7f528");
 
-    // Validate the signature
+    // Verify that the anonymous signer signed the message.
+    // See <https://alloy.rs/examples/wallets/verify_message/> for reference.
+    let recovered = Signature::from_raw(message_signature)?.recover_address_from_msg(message)?;
+    ensure!(recovered == signer);
+
+    // Validate the attester's signature on the attestation
     attestation_info.validate()?;
     ensure!(attestation_info.signer == attester_address);
     println!("Attestation signature is valid");
@@ -38,8 +54,8 @@ fn main() -> Result<()> {
     ensure!(signed_attestation.version == OffchainAttestationVersion::Version2);
 
     let attestation = serde_json::from_value::<AttestationV2>(signed_attestation.data.message)?;
-    ensure!(attestation.recipient == address);
-    ensure!(attestation.schema == eas_schema_id);
+    ensure!(attestation.recipient == signer);
+    ensure!(attestation.schema == EAS_SCHEMA_ID);
     ensure!(attestation.expirationTime == 0); // ensure never expires
 
     let gm_val = GmSchemaData::abi_decode(&attestation.data)?;
@@ -48,13 +64,34 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-// Verifies a signed message from an address.
-//
-// See <https://alloy.rs/examples/wallets/verify_message/> for reference.
-fn verify_signature(address: &Address, message: &[u8], signature: &[u8]) -> Result<()> {
-    let recovered = Signature::from_raw(signature)?.recover_address_from_msg(message)?;
+fn main() -> Result<()> {
+    let address = address!("0x664C7bA58aEE266307Cac0B5a8555095C1a4f7a0");
+    let message =
+        b"Ranked-choice voting is the single most important democracy reform needed in the US";
+    let signature = hex!(
+        "0x3ef5a537df46dc7113fd0d7bea4534315cac6238897b92dd52fd2fc2ff1bb9fb1b1e9f842836194b139f11e9c0d1edaf5fe32099690af3fc2b766b4f8d13cb111b"
+    );
 
-    ensure!(&recovered == address);
+    // Attestation schema: <https://base.easscan.org/schema/view/0x365f50a2e12159e29b002684b5c42a3c1ab67440b886b66ee9a4a9e8c8f7f528>
+    let attester_address = address!("0xc48117F22c8095504aFCa9795DCCbdA2BF5FBc73");
+    let attestation_str = include_str!("../example-attest.json");
+    let expected_gm = "Binius";
+
+    // Attestation link: <https://base.easscan.org/offchain/url/#attestation=eNqlkktuHDEMRO%2FS64FBUvwuPR3PJYIsKDV5gCABcvxoxhcI4tJCC1HFepS%2BH%2FgGb3jcDmcZe4M%2FTPAPInwVJxhdYILF7MLqfEkBZonzFO1eClmUlbNlVXdoYeu16yJg%2BcuEBkYRtegAih4Dhyp00VU8LvFwaVuYa7vhpd7YXD1tHxQy7djkT5%2FFjmgPouUQIsD5ODMs5Nt5zuud7g953Jd9YhYwR%2B4uYw1noL0qdmNKnzG1J%2Bczc6NLrERzHzaLXFNnoMrLZKi0QFIhoWyGmHsy%2BkRfTDl25qnGDNNdp2pVJGeUL29roU98VT5tvovnxwfpnoGdueAu6bIhQk5Mbks4bmg6GJWDb7BvHrdfP3%2FX0wK%2BJIIvSpl0P6yGiY3%2FMTg2Dz1BYs4rrb2vsaLXlVWKC9aYcoV66dBmS5FlRKNkfyIP6GGJq3Lx8eMvWFGrgQ%3D%3D>
+    let attestation_info = serde_json::from_str::<AttestationInfo>(attestation_str)?;
+
+    let inout = AttestedMessageInout {
+        attester_address,
+        message,
+        expected_gm,
+    };
+    let witness = AttestedMessageWitness {
+        signer: address,
+        message_signature: &signature,
+        attestation_info,
+    };
+
+    naively_verify_attested_message(inout, witness)?;
 
     Ok(())
 }
